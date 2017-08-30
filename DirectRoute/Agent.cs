2b364479127aface8a541a31a5ad45d561b34da5
Brian Bosak <brielcccc@gmail.com>
2017-08-30 18:01:41 -0500

    Initial commit

---
+++ DirectRoute/Agent.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+namespace DirectRoute
+{
+    class Agent:IComparable<Agent>
+    {
+        HashSet<uint> knownUnreachables = new HashSet<uint>();
+        SortedSet<Agent> connections = new SortedSet<Agent>();
+        Agent[] cachedConnections = null;
+        //List<Agent> connections = new List<Agent>();
+        public uint ID;
+        public Agent(Random mrand)
+        {
+
+            ID = unchecked((uint)mrand.Next(int.MinValue, int.MaxValue));
+        }
+        object searchToken = null;
+        bool GraphSearch(uint id,object token)
+        {
+            if(knownUnreachables.Contains(id))
+            {
+                return false;
+            }
+            foreach(var iable in connections)
+            {
+                if(iable.searchToken == token)
+                {
+                    //Already traversed
+                    continue;
+                }
+                iable.searchToken = token;
+                if(iable.ID == id)
+                {
+                    return true;
+                }
+                iable.GraphSearch(id, token);
+            }
+            knownUnreachables.Add(id);
+            return false;
+        }
+        public bool CanRoute(uint id)
+        {
+            return GraphSearch(id, new object());
+        }
+        public void AddConnection(Agent other)
+        {
+            knownUnreachables.Clear();
+            cachedConnections = null;
+            if(other == this)
+            {
+                return;
+            }
+            connections.Add(other);
+        }
+        public int TryRoute(Agent from,Agent dest,int hops)
+        {
+
+            hops++;
+            if(dest.ID == ID)
+            {
+                return hops;
+            }
+            if(hops == 30)
+            {
+                return -1;
+            }
+
+            if(cachedConnections == null)
+            {
+                cachedConnections = connections.ToArray();
+            }
+            if(cachedConnections.Length == 0)
+            {
+                return -1;
+            }
+            int found = Array.BinarySearch(cachedConnections, dest);
+            if(found<0)
+            {
+                found = ~found;
+                if(found == cachedConnections.Length)
+                {
+                    found--;
+                }
+            }
+
+            if(cachedConnections[found] == from)
+            {
+                return -1; //NOTE: This should never happen
+            }
+            return cachedConnections[found].TryRoute(this, dest, hops);
+
+            /*uint bestDiff = uint.MaxValue;
+            Agent candidate = null;
+            foreach(var iable in connections)
+            {
+                if(iable == from)
+                {
+                    continue;
+                }
+                uint diff = iable.ID ^ dest;
+                if(diff<bestDiff)
+                {
+                    candidate = iable;
+                    bestDiff = diff;
+                }
+            }
+            if(candidate == null || hops == 30)
+            {
+                return -1; //unroutable
+            }
+
+            return candidate.TryRoute(this, dest, hops);*/
+        }
+
+        public int CompareTo(Agent other)
+        {
+            return ID.CompareTo(other.ID);
+        }
+    }
+}
--- DirectRoute/Program.cs
+++ DirectRoute/Program.cs
@@ -3,13 +3,65 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Diagnostics;
 namespace DirectRoute
 {
     class Program
     {
+        static void PopulateRandom(Agent[] fb)
+        {
+
+            Random shield = new Random(42);
+            for (int i = 0; i < fb.Length; i++)
+            {
+                fb[i] = new Agent(shield);
+
+            }
+            for (int i = 0; i < fb.Length; i++)
+            {
+                for (int c = 0; c < 10; c++ /*This is how C++ was invented*/)
+                {
+                    fb[i].AddConnection(fb[shield.Next(0, fb.Length)]);
+                }
+            }
+        }
+
         static void Main(string[] args)
         {
+            Stopwatch mwatch = new Stopwatch();
+            Agent[] fb = new Agent[500];
+            PopulateRandom(fb);
+
+
+            int maxhops = 0;
+            int reachable = 0;
+            int total = 0;
+            for(int i = 0;i<fb.Length;i++)
+            {
+                for (int c = 0; c < fb.Length; c++ /*this is how C++ was invented*/)
+                {
+                    mwatch.Start();
+                    int hops = fb[i].TryRoute(null, fb[c], 0);
+                    if(hops>maxhops)
+                    {
+                        maxhops = hops;
+                    }
+                    mwatch.Stop();
+                    if(hops == -1)
+                    {
+                        if(fb[i].CanRoute(fb[c].ID))
+                        {
+                            
+                            total++;
+                        }
+                    }else
+                    {
+                        reachable++;
+                        total++;
+                    }
+                }
+            }
+            Console.WriteLine("Success rate: " + ((((double)reachable/total))*100)+"%"+"\nTotal CPU time: "+mwatch.Elapsed+"\nMax hops:" +maxhops);
         }
     }
 }
